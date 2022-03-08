ca9cde735567aaaaa6661d023fec207054fe3f1b
DonquixoteSabo <mixboksu@gmail.com>
2022-03-08 06:38:20 +0100

    feat: Add routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import filterJobs from "./helpers/filterJobs.js";
+import {jobs} from "./data/mockedJobs.js";
 
 const app = express()
 const port = 5000
@@ -13,6 +14,22 @@ app.get('/jobs/all', (req, res) => {
     const isFullTime = req.query.fulltime || null;
     const location = req.query.location || null;
     let filteredJobs = filterJobs({ isFullTime, location });
+
+    return res.json(filteredJobs);
+})
+
+app.get('/jobs/job/:id', (req, res, ctx) => {
+    const { id } = req.params;
+    const job = jobs.filter((job) => job.id.toString() === id);
+
+    return res.json(job[0]);
+})
+
+app.get('/jobs', (req, res) => {
+    const value = req.query.value || null;
+    const isFullTime = req.query.fulltime || null;
+    const location = req.query.location || null;
+    let filteredJobs = filterJobs({ isFullTime, location, value });
 
     return res.json(filteredJobs);
 })
